4615e31ff8c45165baced3c78fbee1cbf8e47ea9
Berwin Lan <74127176+berwinlan@users.noreply.github.com>
2024-04-25 10:59:40 -0400

    Drop unused columns

--- 03-soil/analysis.py
+++ 03-soil/analysis.py
@@ -2,11 +2,24 @@ import pandas as pd
 import numpy as np
 
 def get_data():
-    data = pd.read_csv('xrf_data.csv')
-    data['site'] = data['info'].str.split('_').str[0]
-    data['coordinate'] = data['info'].str.split('_').str[1]
-    data['depth'] = data['info'].str.split('_').str[2]
-    return data
+    df = pd.read_csv('xrf_data.csv')
+
+    # Parse out sampling codes
+    df['site'] = df['info'].str.split('_').str[0]
+    df['coordinate'] = df['info'].str.split('_').str[1]
+    df['depth'] = df['info'].str.split('_').str[2]
+
+    # Grab relevant data
+    cols = df.columns.astype(str).tolist()
+    col_filter = [c for c in cols if 'Concentration' in c or 'Error1s' in c]
+
+    code_cols = [' Units', 'site', 'coordinate', 'depth', 'info']
+
+    cols = df.columns.astype(str).tolist()
+    df_filtered = df[col_filter.extend(code_cols)]
+    df_filtered.head()
+
+    return df
 
 def main():
     data = get_data()
