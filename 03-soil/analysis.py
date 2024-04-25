c57585dad913501629e9a9994897a3e9cc2299f3
Amit Kumar <amitkh@gmail.com>
2024-04-25 12:03:34 -0400

    prelim plots

--- 03-soil/analysis.py
+++ 03-soil/analysis.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
+POLLUTANTS = []
 
 def get_data():
+    global POLLUTANTS
     df = pd.read_csv('xrf_data.csv')
 
     # Parse out sampling codes
@@ -11,27 +15,48 @@ def get_data():
 
     # Grab relevant data
     cols = df.columns.astype(str).tolist()
+    POLLUTANTS = [c for c in cols if 'Concentration' in c]
     col_filter = [c for c in cols if 'Concentration' in c or 'Error1s' in c]
+    df[col_filter] = df[col_filter].apply(pd.to_numeric, errors='coerce') #cast to int
+
+    df = df.sort_values(by=['site', 'depth', 'coordinate'])
 
     code_cols = [' Units', 'site', 'coordinate', 'depth', 'info']
 
-    cols = df.columns.astype(str).tolist()
-    df_filtered = df[col_filter.extend(code_cols)]
-    df_filtered.head()
+    col_filter.extend(code_cols)
+    df_filtered = df[col_filter]
 
-    return df
+    return df_filtered
 
 def get_concentrations(df: pd.DataFrame):
     """
     Return a df with only concentrations.
     """
-    cols = [c for c in cols if 'Concentration' in c].append('info')
+    cols = df.columns.astype(str).tolist()
+    cols = [c for c in cols if 'Concentration' in c] + ['info']
 
     return df[cols]
 
+def generate_plots(df: pd.DataFrame):
+    """
+    df has concentrations
+    """
+    sites = df['info']
+    for p in POLLUTANTS:
+        plt.figure()
+        plt.rcParams["figure.figsize"] = (20, 10)
+        plt.bar(sites, df[p])
+        plt.title("{}".format(p))
+        plt.xlabel("Site Code")
+        plt.ylabel("Concentration (ppm)")
+        plt.xticks(rotation=90)
+        plt.savefig(f"out/{p.replace(' ', '_')}.png", bbox_inches='tight', dpi=300)
+        plt.close()
+
+
 def main():
     data = get_data()
-    print(data.head())
+    generate_plots(data)
 
 if __name__ == '__main__':
     main()
