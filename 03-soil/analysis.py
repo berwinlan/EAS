4b6dd00ca96073cb4cc477e100da59a6006b5b18
Berwin Lan <74127176+berwinlan@users.noreply.github.com>
2024-04-28 22:17:16 -0400

    Clean up code

--- 03-soil/analysis.py
+++ 03-soil/analysis.py
@@ -7,54 +7,38 @@ ERRORS = []
 
 LIMITS = dict()
 
+
 def get_data():
     global POLLUTANTS, ERRORS
-    df = pd.read_csv('xrf_data.csv')
+    df = pd.read_csv("xrf_data.csv")
 
     # Parse out sampling codes
-    df['site'] = df['info'].str.split('_').str[0]
-    df['coordinate'] = df['info'].str.split('_').str[1]
-    df['depth'] = df['info'].str.split('_').str[2]
+    df["site"] = df["info"].str.split("_").str[0]
+    df["coordinate"] = df["info"].str.split("_").str[1]
+    df["depth"] = df["info"].str.split("_").str[2]
 
     # Grab relevant data
     cols = df.columns.astype(str).tolist()
-    POLLUTANTS = [c for c in cols if 'Concentration' in c]
-    ERRORS = [c for c in cols if 'Error1s' in c]
+    POLLUTANTS = [c for c in cols if "Concentration" in c]
+    ERRORS = [c for c in cols if "Error1s" in c]
     col_filter = POLLUTANTS + ERRORS
-    df[col_filter] = df[col_filter].apply(pd.to_numeric, errors='coerce') # cast to int
+    df[col_filter] = df[col_filter].apply(pd.to_numeric, errors="coerce")  # cast to int
 
-    df = df.sort_values(by=['site', 'depth', 'coordinate'])
+    df = df.sort_values(by=["site", "depth", "coordinate"])
 
-    code_cols = [' Units', 'site', 'coordinate', 'depth', 'info']
+    code_cols = [" Units", "site", "coordinate", "depth", "info"]
 
     col_filter.extend(code_cols)
     df_filtered = df[col_filter]
 
     return df_filtered
 
-def get_concentrations(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Return a df with only concentrations.
-    """
-    cols = df.columns.astype(str).tolist()
-    cols = POLLUTANTS + ['info']
-
-    return df[cols]
-
-def get_error1s(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Get only 1SD Error.
-    """
-    cols = df.columns.astype(str).tolist()
-    cols = ERRORS + ['info']
-
-    return df[cols]
 
 def generate_plots(df: pd.DataFrame, debug=False):
     """
-    df has concentrations
+    Plot contaminant concentrations for all pollutants. Includes 1SD error bars.
     """
-    sites = df['info']
+    sites = df["info"]
 
     for p, err in zip(POLLUTANTS, ERRORS):
         plt.figure()
@@ -62,8 +46,11 @@ def generate_plots(df: pd.DataFrame, debug=False):
 
         # Plot data and error bars
         plt.bar(sites, df[p])
-        plt.errorbar(sites, df[p], df[err], ecolor='r', barsabove=True, fmt='r.', markersize=1)
+        plt.errorbar(
+            sites, df[p], df[err], ecolor="r", barsabove=True, fmt="r.", markersize=1
+        )
 
+        # Add an hline for EPA limits if specified
         try:
             lim = LIMITS[p]
             plt.axhline(y=lim)
@@ -80,7 +67,7 @@ def generate_plots(df: pd.DataFrame, debug=False):
             plt.show()
         else:
             # Save plot
-            plt.savefig(f"out/{p.replace(' ', '_')}.png", bbox_inches='tight', dpi=300)
+            plt.savefig(f"out/{p.replace(' ', '_')}.png", bbox_inches="tight", dpi=300)
             plt.close()
 
 
@@ -88,5 +75,6 @@ def main():
     data = get_data()
     generate_plots(data)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
